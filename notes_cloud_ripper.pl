804ff0d8a30ef1bd74135040376c839d26def715
Snarky <thesnarky1@users.noreply.github.com>
2017-10-14 22:05:49 -0400

    Updated ripper to only use memory if the user doesn't specifically ask for a dirty directory

--- notes_cloud_ripper.pl
+++ notes_cloud_ripper.pl
@@ -23,6 +23,7 @@ use Getopt::Long qw(:config no_auto_abbrev);
 my $original_file = "NoteStore.sqlite";
 my $leave_dirty = 0;
 my $help = 0;
+my %notes_to_store;
 
 # Read in options
 GetOptions('file=s' => \$original_file,
@@ -62,24 +63,26 @@ while(my @row = $query_handler->fetchrow_array()) {
   my $z_pk = $row[0];
   my $binary = $row[1];
 
-  # Create filename
-  my $output_file = "note_".$z_pk.".blob.gz";
-  print "\tSaving note $z_pk\n";
+  if($leave_dirty) {
+    # Create filename
+    my $output_file = "note_".$z_pk.".blob.gz";
+    print "\tSaving note $z_pk\n";
 
-  # Save results off to show our work
-  open(OUTPUT, ">$output_file");
-  binmode(OUTPUT);
-  print OUTPUT $binary;
-  close(OUTPUT);
+    # Save results off to show our work
+    open(OUTPUT, ">$output_file");
+    binmode(OUTPUT);
+    print OUTPUT $binary;
+    close(OUTPUT);
+  }
+  my $gunzip_output;
+  gunzip(\$binary => \$gunzip_output);
+  $notes_to_store{$z_pk} = $gunzip_output;
 }
 
-# Gunzip all saved blobs
-print "Extracting gzipped notes\n";
-gunzip '<./note_*.blob.gz>' => '<./note_#1.blob>' or die "Error gunzipping: $GunzipError\n";
-
-# Clean up behind us unless the user wants these files
-if(!$leave_dirty) {
-  unlink(glob "./note_*.blob.gz");
+if($leave_dirty) {
+  # Gunzip all saved blobs
+  print "Extracting gzipped notes\n";
+  gunzip '<./note_*.blob.gz>' => '<./note_#1.blob>' or die "Error gunzipping: $GunzipError\n";
 }
 
 # Create decompressed data column
@@ -92,27 +95,12 @@ print "\tAdded column $decompressed_text_column to store extracted text\n";
 
 print "Parsing and updating Notes\n";
 
-# Work through all the decompressed blob to update the database and do Anything Else
-for $file (glob "note_*.blob") {
-  # Open the file
-  open(NOTE_INPUT, "<$file");
-  binmode(NOTE_INPUT);
-
-  # Get our number
-  my $note_number = $1 if $file =~ /note_(\d+).blob/;
+foreach $note_number (sort(keys(%notes_to_store))) {
+  $binary = $notes_to_store{$note_number};
   print "\tWorking on note $note_number\n";
-
-  # Make sure we snag the entire fil in one go
-  local $/;
-
-  # Read in the binary and close the file
-  my $binary = <NOTE_INPUT>;
-  close(NOTE_INPUT);
 
   # Valid notes appear to start with 08 00 12
   if(is_valid_note($binary)) {
-    # print "$file appears to be a valid note: $binary_header\n";
-
     # Parse out any plaintext at the start
     my $decompressed_text;
     $decompressed_text = get_plaintext($binary);
@@ -125,15 +113,15 @@ for $file (glob "note_*.blob") {
   } else {
     print "\t\tSkipping note $note_number as it appears to be encrypted or otherwise not parsed correctly\n";
   }
-
-  # Clean up behind us unless the user wants to keep the files (sets the --dirty flag)
-  if(!$leave_dirty) {
-    unlink($file);
-  }
 }
 
 # Close database handle
 $dbh->disconnect();
+
+
+####################
+# Functions Follow #
+####################
 
 sub get_plaintext {
   my $input_string = @_[0];
@@ -173,7 +161,7 @@ sub print_usage {
   print "\tperl notes_cloud_ripper.pl [--file=<path to NoteStore.sqlite>] [--dirty] [--help]\n\n";
   print "Options:\n";
   print "\t--file=<path>: Identifies the NoteStore.sqlite file to workon\n";
-  print "\t--dirty: If set, will not delete the .gz and .blob files, letting the user play with them\n";
+  print "\t--dirty: If set, will save .gz and .blob files, letting the user play with them\n";
   print "\t--help: Prints this message\n\n";
   print "Examples:\n";
   print "\tperl notes_cloud_ripper.pl\n";
