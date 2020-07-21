threeplanetssoftware/apple_cloud_notes_parser

commit 8c327cc1e6155d180a90c528152d453b1ec78bfb

Add rudimentary logging to the account and folder objects to better identify how and why they are blank.

lib/AppleNoteStore.rb
@@ -36,17 +36,20 @@ class AppleNoteStore
     @file_path = file_path
     @backup = backup
     @database = SQLite3::Database.new(@file_path.to_s, {results_as_hash: true})
+    @logger = @backup.logger
     @version = version
     @notes = Hash.new()
     @folders = Hash.new()
     @accounts = Hash.new()
     puts "Guessed Notes Version: #{@version}"
+    @logger.debug("Guessed Notes Version: #{@version}")
   end
 
   ##
   # This method does a set of database calls to try to guess the version of Apple Notes we are ripping. 
   # It tries to structure the checks to bail out as each specific version is recognized and then assume we are not it.
   # Helpful changelogs: 
+  # 14 (): ??
   # 13 (https://www.apple.com/ios/ios-13/): Added checklists and shared folders, better search, and gallery 
   # 12 (https://web.archive.org/web/20190909033052/https://www.apple.com/ios/ios-12/): Nothing major 
   # 11 (https://web.archive.org/web/20180828212252/https://www.apple.com/ios/ios-11/): Added document scanner, added tables 
@@ -55,6 +58,7 @@ class AppleNoteStore
   # 8 (https://web.archive.org/web/20150905181128/http://www.apple.com/ios/): Added rich text support, and adding images
   def self.guess_ios_version(database_to_check)
     database_tables = get_database_tables(database_to_check)
+
     ziccloudsyncingobject_columns = get_database_table_columns(database_to_check, "ZICCLOUDSYNCINGOBJECT")
     zicnotedata_columns = get_database_table_columns(database_to_check, "ZICNOTEDATA")
 
@@ -301,6 +305,11 @@ class AppleNoteStore
         rip_account(row["Z_PK"])
       end
     end
+
+    @accounts.each_pair do |key, account|
+      @logger.debug("Rip Accounts final array: #{key} corresponds to #{account.name}")
+    end
+
   end
 
   ##
@@ -309,9 +318,13 @@ class AppleNoteStore
   # If encryption information is present, it adds it with AppleNotesAccount.add_crypto_variables.
   def rip_account(account_id)
   
+    @logger.debug("Rip Account: Calling rip_account on Account ID #{account_id}")
+
     # Set the ZSERVERRECORD column to look at
     server_record_column = "ZSERVERRECORD"
     server_record_column = server_record_column + "DATA" if @version >= 12 # In iOS 11 this was ZSERVERRECORD, in 12 and later it became ZSERVERRECORDDATA
+
+    @logger.debug("Rip Account: Using server_record_column of #{server_record_column}")
 
     # Set the query
     query_string = "SELECT ZICCLOUDSYNCINGOBJECT.ZNAME, ZICCLOUDSYNCINGOBJECT.Z_PK, " + 
@@ -328,6 +341,8 @@ class AppleNoteStore
                      "FROM ZACCOUNT " + 
                      "WHERE ZACCOUNT.Z_PK=?"
     end
+
+    @logger.debug("Rip Account: Query is #{query_string}")
 
     # Run the query
     @database.execute(query_string, account_id) do |row|
@@ -346,6 +361,9 @@ class AppleNoteStore
                                          row["ZCRYPTOITERATIONCOUNT"],
                                          row["ZCRYPTOVERIFIER"])
       end
+
+      @logger.debug("Rip Account: Created account #{tmp_account.name}")
+
       @accounts[account_id] = tmp_account
     end 
   end
@@ -368,6 +386,11 @@ class AppleNoteStore
         rip_folder(row["Z_PK"])
       end
     end
+
+    @folders.each_pair do |key, folder|
+      @logger.debug("Rip Folders final array: #{key} corresponds to #{folder.name}")
+    end
+
   end
 
   ##
@@ -375,6 +398,8 @@ class AppleNoteStore
   # object in ZICCLOUDSYNCINGOBJECTS, identified by Integer +folder_id+, and pulls the needed information to create the object. 
   # This used to use ZICCLOUDSYNCINGOBJECT.ZACCOUNT4, but that value also appears to be duplicated in ZOWNER which goes back further.
   def rip_folder(folder_id)
+
+    @logger.debug("Rip Folder: Calling rip_folder on Folder ID #{folder_id}")
   
     query_string = "SELECT ZICCLOUDSYNCINGOBJECT.ZTITLE2, ZICCLOUDSYNCINGOBJECT.ZOWNER, " + 
                    "ZICCLOUDSYNCINGOBJECT.Z_PK " +
@@ -393,6 +418,9 @@ class AppleNoteStore
       tmp_folder = AppleNotesFolder.new(row["Z_PK"],
                                         row["ZTITLE2"],
                                         get_account(row["ZOWNER"]))
+
+      @logger.debug("Rip Folder: Created folder #{tmp_folder.name}")
+
       @folders[folder_id] = tmp_folder
     end 
   end
@@ -421,6 +449,9 @@ class AppleNoteStore
   # and AppleNotesAccount it is part of. If encryption information is present, it adds 
   # it with AppleNotesAccount.add_crypto_variables.
   def rip_note(note_id)
+  
+    @logger.debug("Rip Note: Ripping note from Note ID #{note_id}")
+
     query_string = "SELECT ZICNOTEDATA.Z_PK, ZICNOTEDATA.ZNOTE, " + 
                    "ZICNOTEDATA.ZCRYPTOINITIALIZATIONVECTOR, ZICNOTEDATA.ZCRYPTOTAG, " + 
                    "ZICNOTEDATA.ZDATA, ZICCLOUDSYNCINGOBJECT.ZCRYPTOVERIFIER, " + 
@@ -467,12 +498,22 @@ class AppleNoteStore
       account_field = "ZACCOUNT"
       note_id_field = "Z_PK"
     end
+   
+    @logger.debug("Rip Note: Query string is #{query_string}") 
+    @logger.debug("Rip Note: account field is #{account_field}")
+    @logger.debug("Rip Note: folder field is #{folder_field}")
 
     # Execute the query
     @database.execute(query_string, note_id) do |row|
       # Create our note
-      tmp_account = get_account(row[account_field])
-      tmp_folder = get_folder(row[folder_field])
+      tmp_account_id = row[account_field]
+      tmp_folder_id = row[folder_field]
+      @logger.debug("Rip Note: Looking up account for #{tmp_account_id}")
+      @logger.debug("Rip Note: Looking up folder for #{tmp_folder_id}")
+      tmp_account = get_account(tmp_account_id)
+      tmp_folder = get_folder(tmp_folder_id)
+      @logger.error("Rip Note: Somehow could not find account!") if !tmp_account
+      @logger.error("Rip Note: Somehow could not find folder!") if !tmp_folder
       tmp_note = AppleNote.new(row["Z_PK"], 
                                row[note_id_field],
                                row["ZTITLE1"], 
@@ -495,7 +536,15 @@ class AppleNoteStore
                                             row["ZCRYPTOWRAPPEDKEY"])
         #tmp_note.decrypt_with_password("password")
       end
-      @notes[tmp_note.note_id] = tmp_note
+      
+      # Only add the note if we have both a folder and account for it, otherwise things blow up
+      if tmp_account and tmp_folder
+        @notes[tmp_note.note_id] = tmp_note
+      else
+        @logger.error("Rip Note: Skipping note #{tmp_note.note_id} due to a missing account.") if !tmp_account
+        @logger.error("Rip Note: Skipping note #{tmp_note.note_id} due to a missing folder.") if !tmp_folder
+        puts "Skipping Note ID #{tmp_note.node_id} due to a missing folder or account, check the debug log for more details."
+      end
     end
   end
 

lib/AppleBackup.rb
@@ -1,4 +1,5 @@
 require 'fileutils'
+require 'logger'
 require 'pathname'
 require_relative 'AppleNote.rb'
 require_relative 'AppleNoteStore.rb'
@@ -11,7 +12,8 @@ class AppleBackup
   attr_accessor :note_stores,
                 :root_folder,
                 :type,
-                :output_folder
+                :output_folder,
+                :logger
 
   # For backups that are created by iTunes and hash the files
   HASHED_BACKUP_TYPE = 1
@@ -34,6 +36,7 @@ class AppleBackup
     @root_folder = root_folder
     @type = type
     @output_folder = output_folder
+    @logger = Logger.new(@output_folder + "debug_log.txt")
     @note_stores = Array.new
     @note_store_modern_location = @output_folder + "NoteStore.sqlite"
     @note_store_legacy_location = @output_folder + "notes.sqlite"
@@ -44,6 +47,7 @@ class AppleBackup
   # No backup on its own is valid, it must be a abckup type that is recognized. In those cases, 
   # instantiate the appropriate child (such as AppleBackupHashed).
   def valid?
+    @logger.error("Returning 'invalid' as no specific type of backup was specified")
     return false
   end
 
@@ -52,6 +56,7 @@ class AppleBackup
   # It expects a String +filename+ to look up. This returns nil by default, and specific types of backups
   # will override this to provide the correct location. 
   def get_real_file_path(filename)
+    @logger.error("Returning nil for get_real_file_path as no specific type of backup was specified")
     return nil
   end
 
@@ -62,7 +67,10 @@ class AppleBackup
   # representing where on this computer the file can be found. Returns a Pathname 
   # representing the relative position of the file in the backup folder.
   def back_up_file(filepath_on_phone, filename_on_phone, filepath_on_disk)
-    return if !filepath_on_disk
+    if !filepath_on_disk
+      @logger.error("Can't call back_up_file with filepath_on_disk that is nil") if @type != SINGLE_FILE_BACKUP_TYPE
+      return
+    end
  
     # Turn the filepath on the phone into a Pathname object for manipulation
     phone_filepath = Pathname.new(filepath_on_phone)
@@ -77,6 +85,7 @@ class AppleBackup
     file_output_directory.mkpath
 
     # Copy the file
+    @logger.debug("Copying #{filepath_on_disk} to #{file_output_directory + filename_on_phone}")
     FileUtils.cp(filepath_on_disk, file_output_directory + filename_on_phone)
 
     # return where we put it 
@@ -98,6 +107,7 @@ class AppleBackup
   # This function kicks off the parsing of notes
   def rip_notes
     @note_stores.each do |note_store|
+      @logger.debug("Apple Backup: Ripping notes from Note Store version #{note_store.version}")
       note_store.rip_all_objects()
     end
   end

notes_cloud_ripper.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'logger'
 require 'optparse'
 require 'pathname'
 require_relative 'lib/AppleBackup.rb'
@@ -79,6 +80,9 @@ end
 
 puts "Storing the results in #{output_directory}\n\n"
 
+# Create the Logger
+logger = Logger.new(output_directory + "debug_log.txt")
+
 #
 # Start dealing with the backup
 #
@@ -87,23 +91,29 @@ puts "Storing the results in #{output_directory}\n\n"
 apple_backup = nil
 case backup_type
   when AppleBackup::HASHED_BACKUP_TYPE
+    logger.debug("User asserted this is a HASHED_BACKUP")
     apple_backup = AppleBackupHashed.new(target_directory, output_directory)
   when AppleBackup::PHYSICAL_BACKUP_TYPE
+    logger.debug("User asserted this is a PHYSICAL_BACKUP")
     apple_backup = AppleBackupPhysical.new(target_directory, output_directory)
   when AppleBackup::SINGLE_FILE_BACKUP_TYPE
+    logger.debug("User asserted this is a SINGLE_FILE_BACKUP")
     apple_backup = AppleBackupFile.new(target_directory, output_directory)
   when AppleBackup::MAC_BACKUP_TYPE
+    logger.debug("User asserted this is a MAC_BACKUP")
     apple_backup = AppleBackupMac.new(target_directory, output_directory)
 end
 
 # Check for a valid AppleBackup, if it is ready, rip the notes and spit out CSVs
 if apple_backup and apple_backup.valid? and apple_backup.note_stores.first.valid_notes?
 
+  logger.debug("Backup is valid, ripping notes")
   #Tell the backup to rip notes
   apple_backup.rip_notes
 
   # Tell the AppleNoteStore to add plaintext to the database
   apple_backup.note_stores.each do |note_store|
+    logger.debug("Adding plaintext to #{note_store}")
     note_store.add_plain_text_to_database
   end
 
@@ -113,21 +123,27 @@ if apple_backup and apple_backup.valid? and apple_backup.note_stores.first.valid
 
   # Make a separate folder to hold the CSVs for cleanliness
   csv_directory = output_directory + "csv"
+  logger.debug("Creating CSV output folder: #{csv_directory}")
   csv_directory.mkpath
 
   # Make a separate folder to hold the HTML
   html_directory = output_directory + "html"
+  logger.debug("Creating HTML output folder: #{html_directory}")
   html_directory.mkpath
 
   backup_number = 1
   apple_backup.note_stores.each do |note_store|
 
+    logger.debug("Working on output for version #{note_store.version} note store #{note_store}")
+
     # Write out the HTML summary
+    logger.debug("Writing HTML for Note Store")
     File.open(html_directory + "all_notes_#{backup_number}.html", "wb") do |file|
       file.write(note_store.generate_html)
     end
 
     # Create a CSV of the AppleNotesAccount objects
+    logger.debug("Writing CSV for accounts")
     CSV.open(csv_directory + "note_store_accounts_#{backup_number}.csv", "wb", force_quotes: true) do |csv|
       note_store.get_account_csv.each do |csv_line|
         csv << csv_line
@@ -135,6 +151,7 @@ if apple_backup and apple_backup.valid? and apple_backup.note_stores.first.valid
     end
 
     # Create a CSV of the AppleNotesFolder objects
+    logger.debug("Writing CSV for folders")
     CSV.open(csv_directory + "note_store_folders_#{backup_number}.csv", "wb", force_quotes: true) do |csv|
       note_store.get_folder_csv.each do |csv_line|
         csv << csv_line
@@ -142,6 +159,7 @@ if apple_backup and apple_backup.valid? and apple_backup.note_stores.first.valid
     end
 
     # Create a CSV of the AppleNote objects
+    logger.debug("Writing CSV for notes")
     CSV.open(csv_directory + "note_store_notes_#{backup_number}.csv", "wb", force_quotes: true) do |csv|
       note_store.get_note_csv.each do |csv_line|
         csv << csv_line
@@ -149,6 +167,7 @@ if apple_backup and apple_backup.valid? and apple_backup.note_stores.first.valid
     end
 
     # Create a CSV of the AppleNotesEmbeddedObject objects
+    logger.debug("Writing CSV for embedded objects")
     CSV.open(csv_directory + "note_store_embedded_objects_#{backup_number}.csv", "wb", force_quotes: true) do |csv|
       note_store.get_embedded_object_csv.each do |csv_line|
         csv << csv_line
@@ -156,6 +175,7 @@ if apple_backup and apple_backup.valid? and apple_backup.note_stores.first.valid
     end
 
     # Close the note store for cleanliness  
+    logger.debug("Closing version #{note_store.version} note store #{note_store}")
     note_store.close
 
     # Increment counter to prevent overwriting our stuff
@@ -171,4 +191,5 @@ else
   exit
 end
 
+logger.debug("Finished")
 puts "\nSuccessfully finished at #{DateTime.now.strftime("%c")}"

commit dd33f55617ed6819b91beb8b7d2adc2e87f2f4bf

Fixes typo in AppleNoteStore introduced in 8c327cc1e6155d180a90c528152d453b1ec78bfb.

lib/AppleNoteStore.rb
@@ -543,7 +543,7 @@ class AppleNoteStore
       else
         @logger.error("Rip Note: Skipping note #{tmp_note.note_id} due to a missing account.") if !tmp_account
         @logger.error("Rip Note: Skipping note #{tmp_note.note_id} due to a missing folder.") if !tmp_folder
-        puts "Skipping Note ID #{tmp_note.node_id} due to a missing folder or account, check the debug log for more details."
+        puts "Skipping Note ID #{tmp_note.note_id} due to a missing folder or account, check the debug log for more details."
       end
     end
   end