e7f0953bf39d637a2d1938c06ca71d380401760f
Three Planets Software <threeplanetssoftware@users.noreply.github.com>
2020-10-01 21:50:01 -0400

    Add CloudKit data to AppleNotesFolder class and output.

--- lib/AppleNotesFolder.rb
+++ lib/AppleNotesFolder.rb
@@ -1,3 +1,6 @@
+require 'keyed_archive'
+require_relative 'AppleCloudKitShareParticipant'
+
 ##
 # This class represents a folder within Apple Notes.
 # It understands which AppleNotesAccount it belongs to and has
@@ -7,7 +10,8 @@ class AppleNotesFolder
   attr_accessor :primary_key,
                 :name,
                 :account,
-                :notes
+                :notes,
+                :share_participants
 
   ##
   # Creates a new AppleNotesFolder.
@@ -22,6 +26,33 @@ class AppleNotesFolder
     @account = folder_account
     # Uncomment the below line if you want to see the folder names during creation
     #puts "Folder #{@primary_key} is called #{@name}"
+
+    # Tracks the AppleCloudKitParticipants this is shared with
+    @share_participants = Array.new()
+  end
+
+  ##
+  # This method adds CloudKit data to an AppleNotesFolder. It requires 
+  # a binary String +cloudkit_data+ which should 
+  def add_cloudkit_data(cloudkit_data)
+    keyed_archive = KeyedArchive.new(:data => cloudkit_data)
+    unpacked_top = keyed_archive.unpacked_top()
+    if unpacked_top
+      unpacked_top["Participants"]["NS.objects"].each do |participant|
+
+        # Pull out the relevant values
+        participant_email = participant["UserIdentity"]["LookupInfo"]["EmailAddress"]
+        participant_record = participant["UserIdentity"]["UserRecordID"]["RecordName"]
+
+        # Initialize a new AppleCloudKitShareParticipant
+        tmp_participant = AppleCloudKitShareParticipant.new()
+        tmp_participant.record_id = participant_record
+        tmp_participant.email = participant_email
+
+        # Add them to this folder
+        @share_participants.push(tmp_participant)
+      end
+    end
   end
 
   ##
@@ -33,13 +64,14 @@ class AppleNotesFolder
   ##
   # This class method spits out an Array containing the CSV headers needed to describe all of these objects
   def self.to_csv_headers
-    ["Folder Primary Key", "Folder Name", "Number of Notes", "Owning Account ID", "Owning Account Name"]
+    ["Folder Primary Key", "Folder Name", "Number of Notes", "Owning Account ID", "Owning Account Name", "Cloudkit Participants"]
   end
 
   ##
   # This method generates an Array containing the information needed for CSV generation
   def to_csv
-    [@primary_key, @name, @notes.length, @account.primary_key, @account.name]
+    participant_emails = @share_participants.map {|participant| participant.email}.join(",")
+    [@primary_key, @name, @notes.length, @account.primary_key, @account.name, participant_emails]
   end
 
   def generate_html
