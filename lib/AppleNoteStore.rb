13a4e2ca55b1d9ba1e2f06465df9b5af73381b3b
Three Planets Software <threeplanetssoftware@users.noreply.github.com>
2021-03-20 22:24:03 -0400

    Clarified the sqlite warning to make it clear when this isn't a problem.

--- lib/AppleNoteStore.rb
+++ lib/AppleNoteStore.rb
@@ -652,7 +652,7 @@ class AppleNoteStore
         @logger.error("Rip Note: Skipping note #{tmp_note.note_id} due to a missing folder.") if !tmp_folder
         
         if !tmp_account or !tmp_folder
-          @logger.error("Consider running these sqlite queries to take a look yourself, make sure ZDATA is NULL: ")
+          @logger.error("Consider running these sqlite queries to take a look yourself, if ZDATA is NULL, that means you aren't missing anything: ")
           @logger.error("\tSELECT Z_PK, #{account_field}, #{folder_field} FROM ZICCLOUDSYNCINGOBJECT WHERE Z_PK=#{tmp_note.primary_key}")
           @logger.error("\tSELECT #{note_id_field}, ZDATA FROM ZICNOTEDATA WHERE #{note_id_field}=#{tmp_note.note_id}")
         end
