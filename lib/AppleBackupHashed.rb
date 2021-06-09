6bbcf1884f11919c9c351cde611e1f202d794013
Three Planets Software <threeplanetssoftware@users.noreply.github.com>
2021-06-09 09:33:28 -0400

    Modified the initialize function to also look for the hashed wal and shm files, if they exist.

--- lib/AppleBackupHashed.rb
+++ lib/AppleBackupHashed.rb
@@ -29,15 +29,32 @@ class AppleBackupHashed < AppleBackup
         puts "Created a new AppleBackup from iTunes backup: #{@root_folder}"
 
         # Copy the modern NoteStore to our output directory
-        FileUtils.cp(@root_folder + "4f" + "4f98687d8ab0d6d1a371110e6b7300f6e465bef2", @note_store_modern_location)
+        hashed_note_store = @root_folder + "4f" + "4f98687d8ab0d6d1a371110e6b7300f6e465bef2"
+        hashed_note_store_wal = @root_folder + "7d" + "7dc1d0fa6cd437c0ad9b9b573ea59c5e62373e92"
+        hashed_note_store_shm = @root_folder + "55" + "55901f4cbd89916628b4ec30bf19717aca78fb2c"
+
+        FileUtils.cp(hashed_note_store, @note_store_modern_location)
+        FileUtils.cp(hashed_note_store_wal, @output_folder + "NoteStore.sqlite-wal") if hashed_note_store_wal.exist?
+        FileUtils.cp(hashed_note_store_shm, @output_folder + "NoteStore.sqlite-shm") if hashed_note_store_shm.exist?
         modern_note_version = AppleNoteStore.guess_ios_version(@note_store_modern_location)
 
         # Copy the legacy NoteStore to our output directory
-        FileUtils.cp(@root_folder + "ca" + "ca3bc056d4da0bbf88b5fb3be254f3b7147e639c", @note_store_legacy_location)
+        hashed_legacy_note_store = @root_folder + "ca" + "ca3bc056d4da0bbf88b5fb3be254f3b7147e639c"
+        hashed_legacy_note_store_wal = @root_folder + "12" + "12be33d156731173c5ec6ea09ab02f07a98179ed"
+        hashed_legacy_note_store_shm = @root_folder + "ef" + "efaa1bfb59fcb943689733e2ca1595db52462fb9"
+
+        FileUtils.cp(hashed_legacy_note_store, @note_store_legacy_location)
+        FileUtils.cp(hashed_legacy_note_store_wal, @output_folder + "notes.sqlite-wal") if hashed_legacy_note_store_wal.exist?
+        FileUtils.cp(hashed_legacy_note_store_shm, @output_folder + "notes.sqlite-shm") if hashed_legacy_note_store_shm.exist?
         legacy_note_version = AppleNoteStore.guess_ios_version(@note_store_legacy_location)
 
         # Copy the Manifest.db to our output directry in case we want to look up files
-        FileUtils.cp(@root_folder + "Manifest.db", @output_folder + "Manifest.db")
+        manifest_db = @root_folder + "Manifest.db"
+        manifest_db_wal = @root_folder + "Manifest.db-wal"
+        manifest_db_shm = @root_folder + "Manifest.db-shm"
+        FileUtils.cp(manifest_db, @output_folder)
+        FileUtils.cp(manifest_db_wal, @output_folder) if manifest_db_wal.exist?
+        FileUtils.cp(manifest_db_shm, @output_folder) if manifest_db_shm.exist?
 
         # Create the AppleNoteStore objects
         @note_stores.push(AppleNoteStore.new(@note_store_modern_location, self, modern_note_version))
