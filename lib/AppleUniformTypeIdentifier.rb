e4c4bc2068bd5807ee8e9d1b9f69afea8bfc92e5
Three Planets Software <threeplanetssoftware@users.noreply.github.com>
2023-08-31 05:43:31 -0400

    Added org.idpf.epub-container to AppleUniformTypeIdentifier. Guessing it behaves like other documents, need to make sure.

--- lib/AppleUniformTypeIdentifier.rb
+++ lib/AppleUniformTypeIdentifier.rb
@@ -94,6 +94,7 @@ class AppleUniformTypeIdentifier
     return true if @uti == "com.microsoft.excel.xls"
     return true if @uti == "com.microsoft.powerpoint.ppt"
     return true if @uti == "net.openvpn.formats.ovpn"
+    return true if @uti == "org.idpf.epub-container"
     return true if @uti == "org.oasis-open.opendocument.text"
     return true if @uti == "org.openxmlformats.wordprocessingml.document"
     return false
