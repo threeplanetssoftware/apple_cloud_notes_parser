2374657284a9ffdf798da0c303bc5fc896fa1585
Three Planets Software <threeplanetssoftware@users.noreply.github.com>
2024-01-03 22:30:54 -0500

    Revert "Updated how characters are counted on their way into htmlify_document. This now uses grapheme_clusters to get a count, instead of assuming everything above code point 65535 ends up being double."
    This reverts commit 8c5d379d55ff56545348d16704653dab8e0d71e7.
    
    This appears to have introduced more errors than it fixed.

--- lib/AppleNote.rb
+++ lib/AppleNote.rb
@@ -606,10 +606,19 @@ class AppleNote < AppleCloudKitRecord
       else # We must have text to parse
 
         # Add in the slice of text represented by this run
-        array_to_add = note_text.grapheme_clusters[current_index, note_part.length]
-        slice_to_add = ""
-        slice_to_add = array_to_add.join if (array_to_add and array_to_add.length > 0)
+        slice_to_add = note_text.slice(current_index, note_part.length)
 
+        # Apple seems to be making Emojis and some other characters two characters
+        # this breaks stuff. This is a really hacky solution.
+        double_characters = 0
+        slice_to_add.each_codepoint do |codepoint|
+          double_characters += 1 if codepoint > 65535
+        end
+
+        if double_characters > 0
+          slice_to_add = note_text.slice(current_index, note_part.length - double_characters)
+        end
+       
         # Calculate what the previous and next attribute runs are 
         previous_run = nil
         previous_run = condensed_attribute_runs[attribute_run_index - 1] if attribute_run_index > 0
@@ -620,7 +629,7 @@ class AppleNote < AppleCloudKitRecord
         note_part.generate_html(slice_to_add, node)
 
         # Increment our counter to be sure we don't loop infinitely
-        current_index += (note_part.length)
+        current_index += (note_part.length - double_characters)
 
       end
 
