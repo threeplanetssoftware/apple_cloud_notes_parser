120ab56ecea1a19104bd738d26903682e1b7f80e
Three Planets Software <threeplanetssoftware@users.noreply.github.com>
2024-08-06 22:26:48 -0400

    Add build script to create all Docker versions at once to help with testing automation.

---
+++ docker_scripts/build_all.sh
@@ -0,0 +1,11 @@
+# Ruby 3.3
+docker build -f dockerfiles/Dockerfile-3.3 -t apple_notes_cloud_parser:ruby-3.3 .
+
+# Ruby 3.2
+docker build -f dockerfiles/Dockerfile-3.2 -t apple_notes_cloud_parser:ruby-3.2 .
+
+# Ruby 3.1
+docker build -f dockerfiles/Dockerfile-3.1 -t apple_notes_cloud_parser:ruby-3.1 .
+
+# Ruby 3.0
+docker build -f dockerfiles/Dockerfile-3.0 -t apple_notes_cloud_parser:ruby-3.0 .
