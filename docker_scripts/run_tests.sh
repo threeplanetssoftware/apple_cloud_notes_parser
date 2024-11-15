7b2da905ff39261ec44d940f5aa81fb9284baa06
Three Planets Software <threeplanetssoftware@users.noreply.github.com>
2024-11-15 09:14:26 -0500

    Updated Dockerfiles to build test images.

---
+++ docker_scripts/run_tests.sh
@@ -0,0 +1,14 @@
+for VERSION in "3.0" "3.1" "3.2" "3.3"
+do
+	IMAGE_NAME="apple_notes_cloud_parser:test-"$VERSION
+	CONTAINER_NAME="apple_cloud_notes_parser-test"
+
+	echo "\n\n###############################\nUsing Docker to test notes_cloud_parser.rb on Ruby "$VERSION"\n###############################\n\n"
+	$VERSION
+
+	docker run --rm --name \
+	  $CONTAINER_NAME \
+	  --volume "$(pwd):/data:ro" \
+	  --volume "$(pwd)/output:/app/output" \
+	  $IMAGE_NAME
+done
